df55567404e5d714509e326c457d2e25e90a3d3a
munakata.hisashi <h.grasshopper2@gmail.com>
2024-04-16 23:52:08 +0900

    Initial Commit

---
+++ GithubClientApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GithubClientApp
+//
+//  Created by 宗像恒 on 2024/04/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GithubClientApp/GithubClientAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubClientAppApp.swift
+//  GithubClientApp
+//
+//  Created by 宗像恒 on 2024/04/16.
+//
+
+import SwiftUI
+
+@main
+struct GithubClientAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
